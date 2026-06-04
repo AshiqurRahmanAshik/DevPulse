e1854c96fc0d07060e62ebbbb0ee10d5542377cf
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-04 19:23:06 +0600

    Add env file for secret key

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,22 @@ import express, {
   type Response,
 } from "express";
 import { Pool } from "pg";
+import config from "./config/config";
 
 const app: Application = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Express Server", author: "Next Level" });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
