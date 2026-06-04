a85bfb07664ba3393822c67e422280dedc79bbc4
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-04 19:24:06 +0600

    Create a user table in NeonDB

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,26 @@ const pool = new Pool({
   connectionString: config.connection_string,
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor'
+      CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Express Server", author: "Next Level" });
 });
