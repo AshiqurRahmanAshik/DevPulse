06c25cdafba971b043b9e0f4c6c86f7b8bfe88f5
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-05 07:50:54 +0600

    Refactor the code from server to app and db

---
+++ src/db/db.ts
@@ -0,0 +1,25 @@
+import { Pool } from "pg";
+import config from "../config/config";
+
+export const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor'
+      CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
+  } catch (error) {
+    console.log(error);
+  }
+};
